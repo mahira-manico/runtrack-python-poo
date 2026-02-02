79f8501529eae9148d44b096b3d1ebdfc280e4fe
mahira-manico <mahira.manico@laplateforme.io>
2026-02-02 12:02:38 +0100

    feat: realization of third excercise and add of commentaries

--- day01/job01.py
+++ day01/job01.py
@@ -1,7 +1,12 @@
+#creation of a class
 class Operation:
-    def __init__(self,number1,number2):
+    #build method with "init"
+    def __init__(self,number1,number2): #add of attribute
         self.number1=number1
         self.number2=number2
 
+#instance creation
 my_number=Operation(1,2)
+
+#print doesn't print since str build isn't initalized
 print(my_number)
--- day01/job02.py
+++ day01/job02.py
@@ -2,7 +2,8 @@ class Operation:
     def __init__(self, number1,number2):
         self.number1=number1
         self.number2=number2
-
+    
+    #creation of str build method witch allow to print result to terminal
     def __str__(self):
         return f"Number1 is {self.number1}\nNumber2 is {self.number2}"
 

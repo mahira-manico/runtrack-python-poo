46c511b8ceadfe88660f5fa1067c2ca7576b8c0f
mahira-manico <mahira.manico@laplateforme.io>
2026-02-02 11:49:42 +0100

    feat: realization of job02

---
+++ day01/job02.py
@@ -0,0 +1,10 @@
+class Operation:
+    def __init__(self, number1,number2):
+        self.number1=number1
+        self.number2=number2
+
+    def __str__(self):
+        return f"Number1 is {self.number1}\nNumber2 is {self.number2}"
+
+my_number=Operation(10,2)
+print(my_number)

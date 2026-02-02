d825bbf9d74c80d2ed4c28284eb0b025818e3f6f
mahira-manico <mahira.manico@laplateforme.io>
2026-02-02 12:53:19 +0100

    doc: add of commentaries for job05

--- day01/job05.py
+++ day01/job05.py
@@ -1,28 +1,30 @@
+#Creation of class
 class Point:
-    def __init__(self,x,y):
-        self.x=x
+    def __init__(self,x,y): #Using of build method "__init__"
+        self.x=x #Creation of variables
         self.y=y
     
-    def DisplayPoints(self):
+    #Creation of methods
+    def DisplayPoints(self): #Display x and y values
         return f"x={self.x} y={self.y}"
     
-    def DisplayX(self):
+    def DisplayX(self): #Display X value only
         return f"x is {self.x}"
 
-    def DisplayY(self):
+    def DisplayY(self): #Display Y value only
         return f"y is {self.y}"
     
-    def ChangeY(self,changeY):
+    def ChangeY(self,changeY): #Change Y value
         self.y=changeY
     
-    def ChangeX(self,changeX):
+    def ChangeX(self,changeX): #Change X value
         self.x=changeX
 
-initializing=Point(10,20)
+initializing=Point(10,20) #Creation of instance 
 
-display=initializing.DisplayPoints()
+display=initializing.DisplayPoints() #Call of method to display all points
 
-change1=initializing.ChangeX(50)
+change1=initializing.ChangeX(50) #Call of method to change value
 
 display2=initializing.DisplayPoints()
 
@@ -30,7 +32,8 @@ change2=initializing.ChangeY(30)
 
 display3=initializing.DisplayPoints()
 
-display4=initializing.DisplayY()
-
+display4=initializing.DisplayY() #Call of method to display one value
+ 
+#Display of all changes on shell
 print(f" Initialization: {display}\n Change of X: {display2}\n Change of Y: {display3}\n Display of Y: {display4}")
 
